eb6144b05714754486579a9ddb1476ad684219ce
Ilya Rypinski <iripinskij@gmail.com>
2020-02-17 19:10:49 +0300

    joi validation added

---
+++ src/middleware/joi.js
@@ -0,0 +1,19 @@
+const validate = (schema, validateData) => {
+    try {
+        const error = schema.validate(validateData);
+        console.log(error);
+        console.log(!error);
+        return !error;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+};
+
+module.exports = objectOfValidation => (req, res, next) => {
+
+    if(validate(objectOfValidation.body, req.body))
+        if(validate(objectOfValidation.params, req.params))
+                next();
+    return res.status(404).json("validateError");
+};
--- src/routes/passport.js
+++ src/routes/passport.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const validator = require("../middleware/joi");
+const schemas = require("../middleware/validation schemas/schemas");
+
 const isAuthenticated = require('../middleware/isAuthenticated');
 const isNotAuthenticated = require('../middleware/isNotAuthenticated');
 
 const Passport = require('../controllers/PassportController');
 const passportController = new Passport();
 
-router.post('/login', isNotAuthenticated, passportController.login);
-router.post('/register', isNotAuthenticated, passportController.register);
+router.post(
+    '/login',
+    isNotAuthenticated,
+    validator({
+        body: schemas.login
+    }),
+    passportController.login);
+
+router.post('/register',
+    isNotAuthenticated,
+    validator({
+    body: schemas.registration
+}),
+    passportController.register);
 
 router.get('/logout', isAuthenticated, passportController.logout);
 
